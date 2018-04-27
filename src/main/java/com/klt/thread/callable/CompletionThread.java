61c1092251faecdd35663ce469151595d1f6a620
gegela <1367260592@qq.com>
2018-04-27 10:48:02 +0800

    文件提交

--- src/main/java/com/klt/thread/callable/CompletionThread.java
+++ src/main/java/com/klt/thread/callable/CompletionThread.java
@@ -5,7 +5,6 @@ import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class CompletionThread {
 	public static void main(String[] args) throws Exception {
@@ -29,6 +28,8 @@ public class CompletionThread {
 		for(int i=1;i<11;i++){
 			System.out.println(completionService.take().get());
 		}
+		
+		es.shutdown();
 		
 		
 	}
